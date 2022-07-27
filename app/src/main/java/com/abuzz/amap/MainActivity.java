c32252f5786befb187aae9cff1e3448cf0624e1c
mishal-a7m <mishalahmed98@gmail.com>
2022-07-27 21:21:36 +0530

    Updated coordinates logic.

--- app/src/main/java/com/abuzz/amap/MainActivity.java
+++ app/src/main/java/com/abuzz/amap/MainActivity.java
@@ -17,7 +17,6 @@ import com.indooratlas.android.sdk.IALocationRequest;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
 public class MainActivity extends AppCompatActivity  {
 
     private class corner{
@@ -52,40 +51,36 @@ public class MainActivity extends AppCompatActivity  {
     public corner topLeft;
     public corner bottomRight;
 
-    public double[] coordinates;
+    public double[] coordinates = new double[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
         ActivityCompat.requestPermissions(this,PERMISSIONS_ALL,REQUEST_PERMISSIONS);
         setContentView(R.layout.activity_main);
 
-       topLeft = new corner(-5835,46063,25.21577789088,55.27965678284);
+
+        topLeft = new corner(-5835,46063,25.21577789088,55.27965678284);
         bottomRight = new corner(22481, 17722, 25.21318461787,55.28253282524);
 
-
-
-        coordinates = latlngToXY( 25.21511,55.28118);
-
-
+        coordinates = latlngToXY(25.214441, 55.281049);
 
         mIALocationManager = IALocationManager.create(this);
 
         mIALocationListener = new IALocationListener() {
             @Override
             public void onLocationChanged(IALocation iaLocation) {
-                //getLat() getLon() function returns double.
-                log("The current lat-lon-alt are as follows "+ iaLocation.getLatitude()+" "+iaLocation.getLongitude()+" "+iaLocation.getAltitude());
 
+                log("The current lat-lon-alt are as follows "+ iaLocation.getLatitude()+" "+iaLocation.getLongitude()+" "+iaLocation.getAltitude());
+                coordinates = latlngToXY(iaLocation.getLatitude(), iaLocation.getLongitude());
                 Toast.makeText(getApplicationContext(),iaLocation.getLatitude()+" "+iaLocation.getLongitude()+" "+iaLocation.getAltitude(),Toast.LENGTH_SHORT)
                               .show();
             }
 
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle) {
+                log(s+i);
 
             }
         };
@@ -133,42 +128,32 @@ public class MainActivity extends AppCompatActivity  {
 
     // FAKE simulation of user moving
     private int cur_step = 0;
-    private double[] from_pos = new double[]{  14478, 32373, -200 }; // lon,lat
-    private double[] to_pos = new double[]{ 35157, 34942, -200 };
-    private boolean simulationStarted = false;
 
-//    55.28172,25.21478
 
     private double[] latlngToXY(double lat, double lng){
         double perX = (lng - topLeft.lon)/(bottomRight.lon - topLeft.lon);
         double perY = (lat - topLeft.lat)/(bottomRight.lat - topLeft.lat);
 
-        double[] coordinates = new double[2];
+        coordinates[0] = topLeft.x + (bottomRight.x - topLeft.x) * perX  ; //lon
+        coordinates[1] = topLeft.y + (bottomRight.y - topLeft.y) * perY  ; //lat
 
-        coordinates[0] = topLeft.x + (bottomRight.x - topLeft.x) * perX; //lon
-        coordinates[1] = topLeft.y + (bottomRight.y - topLeft.y) * perY; //lat
-
-        log(coordinates[0]);
-        log("Hello");
-        log(coordinates[1]);
-
+       // log(coordinates[0] + "  " + coordinates[1]);
         return coordinates;
-
     }
 
-    private void startLocationSimulationLoop() {
-        if(!simulationStarted) {
-            simulationStarted = true;
-//
 
-            Runnable positionWebViewRunnable = () -> {
+    private void startLocationSimulationLoop() {
+
+        Runnable positionWebViewRunnable = () -> {
+
                 AMapViewInstance.getInstance().getAmap().setLonLatCurrentPosition(
                         coordinates[0],
                         coordinates[1],
-                        from_pos[2] + cur_step*(to_pos[2] - from_pos[2])/40,
+                        0.0,
                         cur_zone, null);
-                log("Thread running"+ cur_step);
-               // cur_step = (cur_step + 1) % 40;
+
+                log(coordinates[0] + "thread" + coordinates[1]);
+
             };
 
             TimerTask positionTimerTask = new TimerTask() {
@@ -183,17 +168,11 @@ public class MainActivity extends AppCompatActivity  {
             new Timer().scheduleAtFixedRate(positionTimerTask, 1000, 1000);
 
             Toast.makeText(this, "Location Simulation started", Toast.LENGTH_SHORT).show();
-        }
     }
+
 
     private void log(Object object){
         System.out.println("FATAL::: "+object);
     }
 }
 
-// from_pos[0] + cur_step*(to_pos[0] - from_pos[0])/40,
-//         from_pos[1] + cur_step*(to_pos[1] - from_pos[1])/40,
-//         from_pos[2] + cur_step*(to_pos[2] - from_pos[2])/40,
-
-//    private double[] from_pos = new double[]{ 8543, 32373, -200 };
-//    private double[] to_pos = new double[]{ 10125, 34942, -200 };
